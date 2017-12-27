a9773260ecd2653b172b90f811576f824fdd35cc
JaeyongHan <proghjy@gmail.com>
2017-12-27 20:49:17 +0900

    config eslint

--- public/es6/test.js
+++ public/es6/test.js
@@ -10,7 +10,7 @@ function say({ subject, verb, object}) {
 	// es6 기능: 템플릿 문자열
 	// 아래 사용한 것은 따옴표가 아니라 백틱(`), 즉
 	// 키보드의 탭 키 바로 위에 있는 문자 입니다.
-	console.log(`${subject}${verb}${objejct}`);
+	console.log(`${subject} ${verb} ${object}`);
 }
 
 // es6 기능: for..of
